0b0657d9d6139a39d41a5f3679360f95d3bc6405
Spartoons <aranoliveras@gmail.com>
2024-05-14 17:43:10 +0200

    actualización 14:42 14/05/2024 :100:

--- project2/gen_tree.c
+++ project2/gen_tree.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct CivilRegistry
+{
+    int city_id;
+    char city_name[20];
+    char mother_name[10];
+    char father_name[10];
+    int mother_parents_city_id;
+    int father_parents_city_id;
+};
+
+
 typedef struct TreeNode{
-    int data;
-    struct TreeNode *left;
-    struct TreeNode *right;
+    char mother_name[10];
+    char father_name[10];
+    int city_id;
+    struct TreeNode *mother_parents;
+    struct TreeNode *father_parents;
 }TreeNode;
+
+// DFS:
+
+
+
+// BFS:
+
+
+int main(){
+    return 0;
+}
